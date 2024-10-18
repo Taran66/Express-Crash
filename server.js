19a4b88cb0ef56b0e17912b1b59f9e7c247f60b1
Taran66 <taranpreet1911@gmail.com>
2024-10-18 17:45:33 +0530

    added new

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require('express')
+import express from 'express'
+import path from 'path';
+import posts from './routes/posts.js'
+import logger from './middleware/logger.js'
+import errorHandler from './middleware/error.js';
+const port = process.env.PORT || 8000
 
-const path = require('path')
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+//Logger middleware
+app.use(logger);
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
 
-let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'}
-];
 
-app.get('/api/posts', (req, res) => {
-    res.json(posts)
-})
+//Routes
+app.use('/api/posts', posts)
 
-app.listen(8000, () => console.log(`Server is running on port 8000`))
+// Error handler
+app.use(errorHandler);
+
+app.listen(8000, () => console.log(`Server is running on port ${port}`)) 
